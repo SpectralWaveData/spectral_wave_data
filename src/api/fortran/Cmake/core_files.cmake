397b524b250b9203146c55e561d4bb4f84f483ac
Tormod Landet <Tormod.Landet@dnvgl.com>
2020-09-01 16:41:47 +0200

    De-duplicate CMake files

---
+++ src/api/fortran/Cmake/core_files.cmake
@@ -0,0 +1,21 @@
+# All the Fortran implementation source files except kind_values.f90
+# You need to define DIR_SRC_API_F before include()-ing this file
+set(SRC_CORE
+  ${DIR_SRC_API_F}/open_swd_file.f90
+  ${DIR_SRC_API_F}/spectral_interpolation.f90
+  ${DIR_SRC_API_F}/spectral_wave_data.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_allocate.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_c.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_error.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_1_impl_1.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_2_impl_1.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_3_impl_1.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_4_impl_1.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_4_impl_2.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_5_impl_1.f90
+  ${DIR_SRC_API_F}/spectral_wave_data_shape_6_impl_1.f90
+  ${DIR_SRC_API_F}/swd_write_shape_1_or_2.f90
+  ${DIR_SRC_API_F}/swd_write_shape_3.f90
+  ${DIR_SRC_API_F}/swd_write_shape_4_or_5.f90
+  ${DIR_SRC_API_F}/swd_write_shape_6.f90
+  ${DIR_SRC_API_F}/swd_version.f90)
