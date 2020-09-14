6b747365671ae1f0fff03ef7eafb3f4255e2801f
Tormod Landet <Tormod.Landet@dnvgl.com>
2020-09-14 13:13:52 +0200

    Capital F in file listing enables the preprocessor

--- src/api/fortran/Cmake/core_files.cmake
+++ src/api/fortran/Cmake/core_files.cmake
@@ -1,7 +1,7 @@
 # All the Fortran implementation source files except kind_values.f90
 # You need to define DIR_SRC_API_F before include()-ing this file
 set(SRC_CORE
-  ${DIR_SRC_API_F}/open_swd_file.f90
+  ${DIR_SRC_API_F}/open_swd_file.F90
   ${DIR_SRC_API_F}/spectral_interpolation.f90
   ${DIR_SRC_API_F}/spectral_wave_data.f90
   ${DIR_SRC_API_F}/spectral_wave_data_allocate.f90
@@ -19,12 +19,3 @@ set(SRC_CORE
   ${DIR_SRC_API_F}/swd_write_shape_4_or_5.f90
   ${DIR_SRC_API_F}/swd_write_shape_6.f90
   ${DIR_SRC_API_F}/swd_version.f90)
-
-# Enable C++ style pre-processor directives for files that use them
-if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
-    set_source_files_properties(
-      ${DIR_SRC_API_F}/open_swd_file.f90
-      PROPERTIES
-      COMPILE_FLAGS
-      -cpp)
-endif()
