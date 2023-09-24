6f0267fd747c8568962d88f7990f5f77b02e614a
Jens B. Helmers <jens.bloch.helmers@dnv.com>
2023-09-24 21:25:33 +0200

    Adjust compiler flag for local Linux distro (python)

--- src/api/python/Cmake/CMakeBuild_Linux.bash
+++ src/api/python/Cmake/CMakeBuild_Linux.bash
@@ -1,5 +1,5 @@
 rm -rf Build_Linux
 mkdir Build_Linux
 cd Build_Linux
-cmake -DCMAKE_Fortran_COMPILER=gfortran-11 -DSTATIC=OFF ..
+cmake -DCMAKE_Fortran_COMPILER=gfortran-11 -DCMAKE_BUILD_TYPE=Release ..
 make
